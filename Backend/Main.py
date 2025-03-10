0eb24318b89d66287273428e736bb65ecdb38094
Coding-Phantom <vudavidwork5564@gmail.com>
2025-03-09 18:04:01 -0700

    Updated Card.py and added Slapjack rules utility method

---
+++ Backend/Main.py
@@ -0,0 +1,8 @@
+from Card import Card
+
+from SlapjackRules import SlapjackRules
+
+da_card = Card("Queen", "Hearts")
+
+print(SlapjackRules.is_jack(da_card.rank))
+
---
+++ Backend/SlapjackRules.py
@@ -0,0 +1,6 @@
+class SlapjackRules:
+
+    """Basic check for checking if it's a Jack, default setting"""
+    def is_jack(card):
+        da_card = card.lower()
+        return da_card == "jack"
