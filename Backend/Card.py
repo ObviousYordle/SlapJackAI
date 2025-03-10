b6f147aa2b7dce69179dbae2b4a9ff744fee7d63
Coding-Phantom <vudavidwork5564@gmail.com>
2025-03-09 18:12:00 -0700

    Updated card and added gitignore

--- Backend/Card.py
+++ Backend/Card.py
@@ -1,5 +1,23 @@
 class Card():
-    def __init__(self, num, suite):
-        self.num = num
-        self.suite = suite
+    def __init__(self, rank, suit):
+        self._rank = rank
+        self._suit = suit
 
+    @property
+    def rank(self):
+        return self._rank
+
+    @property
+    def suit(self):
+        return self._suit
+
+    @rank.setter
+    def rank(self, value):
+        self._rank = value
+
+    @suit.setter
+    def suit(self, value):
+        self._suit = value
+
+    def __str__(self):
+        return f"{self._rank} of {self._suit}"
