c9cd948052b01d3b2af7e34502b18fe29dab5be9
Coding-Phantom <vudavidwork5564@gmail.com>
2025-03-26 15:43:26 -0700

    Added card visual

--- Backend/Deck.py
+++ Backend/Deck.py
@@ -3,8 +3,8 @@ import random
 
 class Deck:
     def __init__(self):
-        self.suits = ['spades', 'clubs', 'diamonds', 'hearts']
-        self.ranks = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king']
+        self.suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
+        self.ranks = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 
         """Create a tuple of each cards to make a deck of 52 cards"""
         self.deck = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
